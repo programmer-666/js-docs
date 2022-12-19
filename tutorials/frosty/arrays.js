0a428e7565213cff4fe461e60d844c93647c4808
programmer-666 <suharslan@protonmail.ch>
2022-12-19 08:37:47 +0300

    update

--- tutorials/frosty/arrays.js
+++ tutorials/frosty/arrays.js
@@ -1,3 +1,21 @@
 const array = "Hello world, lorem ipsum dolor sit amet.".split(" ");
 
 console.log(array);
+
+/*
+
+concat(): İki veya daha fazla array'i birleştirir ve yeni bir array oluşturur.
+join(): Array elemanlarını birleştirir ve bir dize oluşturur.
+pop(): Array'in son elemanını çıkartır ve bu elemanı döndürür.
+push(): Array'e yeni elemanlar ekler.
+reverse(): Array elemanlarını ters çevirir.
+shift(): Array'in ilk elemanını çıkartır ve bu elemanı döndürür.
+slice(): Array'den belirli bir bölümü alır ve yeni bir array oluşturur.
+sort(): Array elemanlarını sıralar.
+splice(): Array'den belirli elemanları çıkartır veya yeni elemanlar ekler.
+unshift(): Array'in başına yeni elemanlar ekler.
+
+*/
+
+array.pop();
+console.log(array);
